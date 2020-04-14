9eb3744737bc2601473e387cce38dbf6be72092f
Arsalan Rashid <39867167+arsalan-rashid@users.noreply.github.com>
2020-04-14 16:39:22 +0100

    updated

--- app.js
+++ app.js
@@ -1,52 +1,69 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+//jshint esversion:6
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/fruitsDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
-// Database Name
-const dbName = 'fruits';
+const fruitSchema = new mongoose.Schema({
+name: String,
+rating: Number,
+review: String
+});
 
-// Create a new MongoClient
-const client = new MongoClient(url,{ useUnifiedTopology: true });
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-
-      findDocuments(db, function() {
-        client.close();
-      });
+const Fruit = mongoose.model("Fruit",fruitSchema);
+const fruit = new Fruit ({
+  name: "Apple",
+  rating: 7,
+  review: "Preety Good."
 });
 
 
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Insert some documents
-  collection.insertMany([
-    {
-      name: "Mango",
-      score:8,
-      review: "Mangos are great"
-    },
-    {
-      name: "Pear",
-      score:6,
-      review: "Pear are hard"
-    }
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
+const kiwi = new Fruit({
+name: "Kiwi",
+rating:10,
+review: "The best fruit!"
+});
+
+const orange = new Fruit({
+name: "Orange",
+rating:4,
+review: "Sour in taste!"
+});
+
+const banana = new Fruit({
+name: "Banana",
+rating:3,
+review: "Good Texture!"
+});
+
+//
+// Fruit.insertMany([kiwi,orange,banana],function(err){
+// if(err){
+//   console.log(err);
+// }else{
+//   console.log("Sucessfully Added");
+// }
+// });
+
+Fruit.find(function(err,fruits){
+if(err){
+  console.log(err);
+}else{
+  console.log(fruits);
+fruits.forEach(function(fruit){
+console.log(fruit.name);
+});
+
+
 }
+
+
+});
+
+
+
+
+
+
 
 const findDocuments = function(db, callback) {
   // Get the documents collection
@@ -55,7 +72,8 @@ const findDocuments = function(db, callback) {
   collection.find({}).toArray(function(err, fruits) {
     assert.equal(err, null);
     console.log("Found the following records");
-    console.log(fruits)
+    console.log(fruits);
     callback(fruits);
+
   });
-}
+};
